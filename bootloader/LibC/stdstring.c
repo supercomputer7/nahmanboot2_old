5d0fc1282bc16e82a3e5afc0cf3610d8bcb487f6
supercomputer7 <liavalb@gmail.com>
2019-10-11 01:11:01 +0300

    LibC: Adding functions to handle strings

---
+++ bootloader/LibC/stdstring.c
@@ -0,0 +1,31 @@
+#include "stdstring.h"
+
+int strncmp(const char *str1, const char *str2,uint16_t len)
+{
+    int i=0;
+    while(i<len)
+    {
+        if(str1[i] == '\0' && str2[i] == '\0')
+            break;
+        if(str1[i] != str2[i])
+            return 0;
+        i++;
+    }
+    return 1;
+}
+void strcopy(char *source, char *dest,uint32_t len)
+{
+    for(uint32_t i=0; i<len; ++i)
+        dest[i] = source[i];
+}
+uint32_t strlen(const char *str)
+{
+    char *p = (char*)str;
+    uint32_t count = 0;
+    while(*p != '\0')
+    {
+        count++;
+        p++;
+    }
+    return count;
+}
---
+++ bootloader/LibC/stdstring.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+int strncmp(const char *str1, const char *str2,uint16_t len);
+void strcopy(char *source, char *dest,uint32_t len);
+uint32_t strlen(const char *str);
