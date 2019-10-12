ce348fbdf3104f879eeb578ac2a57dee3c24126a
supercomputer7 <liavalb@gmail.com>
2019-10-12 12:28:11 +0300

    LibC: Changing return type of strncmp function

--- bootloader/LibC/stdstring.c
+++ bootloader/LibC/stdstring.c
@@ -1,6 +1,6 @@
 #include "stdstring.h"
 
-int strncmp(const char *str1, const char *str2,uint16_t len)
+bool strncmp(const char *str1, const char *str2,uint16_t len)
 {
     int i=0;
     while(i<len)
@@ -8,10 +8,10 @@ int strncmp(const char *str1, const char *str2,uint16_t len)
         if(str1[i] == '\0' && str2[i] == '\0')
             break;
         if(str1[i] != str2[i])
-            return 0;
+            return false;
         i++;
     }
-    return 1;
+    return true;
 }
 void strcopy(char *source, char *dest,uint32_t len)
 {
--- bootloader/LibC/stdstring.h
+++ bootloader/LibC/stdstring.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
-int strncmp(const char *str1, const char *str2,uint16_t len);
+#include <stdbool.h>
+bool strncmp(const char *str1, const char *str2,uint16_t len);
 void strcopy(char *source, char *dest,uint32_t len);
 uint32_t strlen(const char *str);
