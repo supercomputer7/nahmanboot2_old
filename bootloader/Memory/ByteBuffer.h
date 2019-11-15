d7d65494305b2452998daf0f60bad55a281380e9
supercomputer7 <liavalb@gmail.com>
2019-11-15 13:01:26 +0200

    Memory: Adding ByteBuffer class

---
+++ bootloader/Memory/ByteBuffer.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <stdint.h>
+#include <LibC/stdbool.h>
+#include <Memory/malloc.h>
+
+class ByteBuffer {
+
+public:
+    ByteBuffer(void* addr,size_t size)
+    {
+        this->ptr = addr;
+        this->buffer_size = size;
+        this->byte_pointer = 0;
+    }
+    bool is_null()
+    {
+        return !(ptr == nullptr);
+    }
+    size_t get_buffer_size()
+    {
+        return buffer_size;
+    }
+    void null_it()
+    {
+        ptr = nullptr;
+    }
+    void* get_ptr()
+    {
+        return ptr;
+    }
+    uint8_t operator[](int i)
+    {
+        if(i < buffer_size)
+            return static_cast<uint8_t*>(this->ptr)[i];
+        else
+            return 0;
+    }
+    uint8_t get_byte()
+    {
+        uint8_t byte;
+        if(byte_pointer < buffer_size)
+        {
+            byte = static_cast<uint8_t*>(this->ptr)[byte_pointer];
+            this->byte_pointer++;
+        }
+        else
+        {
+            reset_offset_pointer();
+            byte = static_cast<uint8_t*>(this->ptr)[byte_pointer];
+        }
+    }
+    void reset_offset_pointer()
+    {
+        this->byte_pointer = 0;
+    }
+private:
+    void* ptr;
+    uint32_t buffer_size;
+    uint32_t byte_pointer;
+};
